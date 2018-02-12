b8dd8a4c19c0217e1fa1a3d3396520842767a276
Baard H. Rehn Johansen <baard.johansen@bouvet.no>
2018-02-12 12:13:08 +0100

    fix broken default eval

--- service/service.py
+++ service/service.py
@@ -1,11 +1,7 @@
 import os
 import json
 
-env=os.environ.get('EXEC', '''[
-"for k in os.environ:"
-"  print("%s: %s" % (k, os.environ[k]))"
-]
-''')
+env=os.environ.get('EXEC', '["for k in os.environ:","  print(\\"%s: %s\\" % (k, os.environ[k]))"]')
 cmd=json.loads(env)
 if isinstance(cmd, list):
  cmd="\n".join(cmd)
